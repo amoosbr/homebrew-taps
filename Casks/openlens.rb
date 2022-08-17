ab0d614248ee336d41d2542a4b1b65b239de0d65
amoosbr <amoosbr@users.noreply.github.com>
2022-08-17 03:34:56 +0000

    OpenLens version updated to: 6.0.1

--- Casks/openlens.rb
+++ Casks/openlens.rb
@@ -2,12 +2,12 @@ cask "openlens" do
     version "6.0.1"
 
     if OS.mac? && Hardware::CPU.intel?
-      sha256 "9ebd2a23cea45b4e862fd223dd74bab890a1b04181713da104801ecdc76c976f"
+      sha256 "e79df8618902e3cb1b0cd8437420f0a015f4a2954961f70c9a0d264af931566d"
       url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}.dmg"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-      sha256 "39210a871c13775821090b560524e96a66443419b3441150ccb95ad1715ad357"
+      sha256 "79d252a8758f4758d8648fe5e831b5e48c68b6ab73ce8f516597ff5b07f5cdda"
       url "https://github.com/MuhammedKalkan/OpenLens/releases/download/v#{version}/OpenLens-#{version}-arm64.dmg"
     end
 
