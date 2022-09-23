54b7e12aa8a85373202dfa465ad7b3571fa9a93a
amoosbr <amoosbr@users.noreply.github.com>
2022-09-23 03:58:07 +0000

    Ratchet version updated to: 0.3.1

--- Formula/ratchet.rb
+++ Formula/ratchet.rb
@@ -2,17 +2,17 @@
 class Ratchet < Formula
     desc "A tool for securing CI/CD workflows with version pinning."
     homepage "https://github.com/sethvargo/ratchet"
-    version "0.3.0"
+    version "0.3.1"
   
     on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/sethvargo/ratchet/releases/download/v#{version}/ratchet_#{version}_darwin_arm64.tar.gz", :using => CurlDownloadStrategy
-        sha256 "78e17b4267d0d353b01a825a7b52a8f228f9ffb938a5e0a828cbbb7bbe637c46"
+        sha256 "489036637812ad47daf566439789ec76a7302226971421bd9c9b5312e48d9bb3"
   
       end
       if Hardware::CPU.intel?
         url "https://github.com/sethvargo/ratchet/releases/download/v#{version}/ratchet_#{version}_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
-        sha256 "00eec381a0dfa41b94310a0df40b52cb281caf61c663b7d62bc5c79654e5f2a3"
+        sha256 "e6361b5ca57bd50b4442e279a4d1a06f21c5bd0cc224ebfdb2ad0c16ce62d925"
   
       end
     end
@@ -20,12 +20,12 @@ class Ratchet < Formula
     on_linux do
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/sethvargo/ratchet/releases/download/v#{version}/ratchet_#{version}_linux_arm64.tar.gz", :using => CurlDownloadStrategy
-        sha256 "a114fecb06a1ffa5fcf39695e6ffede8dc2bd5617ac33a5095834da7888db4ff"
+        sha256 "0ae92a365b709769b5158a7ec51cada1e1bd4bbb0e9da660a4542f177a6aae23"
   
       end
       if Hardware::CPU.intel?
         url "https://github.com/sethvargo/ratchet/releases/download/v#{version}/ratchet_#{version}_linux_amd64.tar.gz", :using => CurlDownloadStrategy
-        sha256 "6c3a469aae918f177c2dd7a30b0f3fa0262eff9cfbf8ff6146f564047a196f3f"
+        sha256 "7c65dbab567fbc6b91f9a622ab61a4ad61b146ef60726c7faaea7806cd0baa3d"
   
       end
     end
